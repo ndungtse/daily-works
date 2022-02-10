3686f61889df76ebd77eb3821cfb672d122a2719
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-02-10 10:59:59 +0200

    main

---
+++ fop/10.2/for1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(void) {
+                int nCount;
+                for (int nCount = 0; nCount <+ 10; nCount++) {
+                    printf("%d\n", nCount);
+                }
+                    printf("\n");
+}
