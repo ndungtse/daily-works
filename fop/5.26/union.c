e48c26ea6dc0033e606d36217b17382b26733ce2
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-02 21:15:33 +0200

    diffs

--- fop/5.26/union.c
+++ fop/5.26/union.c
@@ -5,11 +5,22 @@ union abc
    int a;
    char b;
 } var;
+struct acb
+{
+   int a;
+   char b;
+} var1;
 
 int main(){
-    var.a =64;
+    var.a =63;
+    var.b=65;
+    var1.a =63;
+    var1.b=65;
     printf("a=%d\n", var.a);
     printf("b=%c\n", var.b);
-    printf("Size is: %ld", sizeof(union abc));
+    printf("Size is: %ld\n", sizeof(union abc));
+    printf("a=%d\n", var1.a);
+    printf("b=%c\n", var1.b);
+    printf("Size is: %ld", sizeof(struct acb));
     return 0;
 }
--- fop/5.5/exercise/filecreate.c
+++ fop/5.5/exercise/filecreate.c
@@ -6,6 +6,8 @@ int main(){
     char fname[81];
     char buffer[101];
     FILE *instream;
+    int n;
+    int i;
 
     instream = fopen("fname.txt", "w");
 
@@ -14,6 +16,17 @@ int main(){
         printf("Unable to to open file %s\n", fname);
         exit(-1);
     }
-    fprintf(instream, "Hello World");
+    printf("Enter a number of lines you want to enter: ");
+    scanf("%d", &n);
+
+    for ( i = 0; i < n; i++)
+    {
+        printf("Enter a line: ");
+        gets(buffer);
+        fprintf(instream, "%s\n", buffer);
+    }
+
+    fprintf(instream, "Hello World\n");
+    fputs("How y'all doing", instream);
     fclose(instream);
 }
