8b2c441b15ccf69b7a05a09e8a48c300f4ef69e7
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-09 08:18:39 +0200

    binary file

---
+++ fop/6.7/read.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int num;
+    FILE *fptr;
+    if ((fptr = fopen("program.bin", "r")) == NULL)
+    {
+        printf("Error! opening file");
+        // Program exits if the file pointer returns NULL.
+        exit(1);
+    }
+    fscanf(fptr, "%d", &num);
+    printf("Value of n=%d", num);
+    fclose(fptr);
+
+    return 0;
+}
