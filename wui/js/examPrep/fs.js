d85e316f896d052902bc130359d73c55dbbac720
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-28 22:40:38 +0200

    jsprep

--- wui/js/examPrep/fs.js
+++ wui/js/examPrep/fs.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 // const { users } = require('./users.json');
 
-fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(JSON.parse(data).users);
-    }
-);
+// fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(JSON.parse(data).users);
+//     }
+// );
+
+console.log(os.platform());
