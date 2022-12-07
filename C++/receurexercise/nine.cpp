12e1b6a908d84384bb5def92afe0e01c703bd7ca
Ndungutse Charles <ndungutsecharles103@gmail.com>
2022-12-07 08:37:21 +0200

    finished recursive exercises

--- C++/receurexercise/nine.cpp
+++ C++/receurexercise/nine.cpp
@@ -2,23 +2,23 @@
 #include <array>
 using namespace std;
 
-int lastIndex(int nums[], int len, int x, int num=-1, int n = 0)
+int firstIndex(int nums[], int len, int x, int n = 0)
 {
-    if(nums[n]== x)
-        num = n;
+    if (nums[n] == x)
+        return n;
 
     if (n == len - 1)
-        return num;
+        return n;
 
     // cout << len << endl;
-    return lastIndex(nums, len, x, num, n + 1);
+    return firstIndex(nums, len, x, n + 1);
 }
 
 int main(int argc, char const *argv[])
 {
     int a[] = {5, 5, 6, 20, 5, 6};
     int size = sizeof(a) / sizeof(int);
-    int last = lastIndex(a, size, 20);
-    cout << last;
+    int first = firstIndex(a, size, 5);
+    cout << first;
     return 0;
 }
--- C++/receurexercise/seven.cpp
+++ C++/receurexercise/seven.cpp
@@ -3,16 +3,28 @@ using namespace std;
 
 string removeConsDup(string str, int i = 0)
 {
+    int count = 1;
+    bool isDup = true;
     if (i == str.length())
         return "\0";
-    if (str[i] == str[i + 1])
-        str[i + 1] = '\0';
+    while (isDup)
+    {
+        bool isEqual = str[i] == str[i + 1];
+        if (str[i] == str[i + count])
+        {
+            str[i + count] = '\0';
+            count++;
+        }
+        else
+            isDup = false;
+    }
+
     return str[i] + removeConsDup(str, i + 1);
 }
 
 int main(int argc, char const *argv[])
 {
-    string str = "aaabbbcccddd";
+    string str = "aabbcccdddd";
     string leftstr = removeConsDup(str);
     cout << leftstr;
     return 0;
